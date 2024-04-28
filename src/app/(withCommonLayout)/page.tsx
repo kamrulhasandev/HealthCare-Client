00c313a407df1318ac769ddc823ef2ff3b3c2ee1
kamrul <kamrul444216@gmail.com>
2024-04-29 02:56:11 +0600

    create top rated doctors section with data fetching

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
+import Specialties from "@/components/UI/HomePage/Specialties/Specialties";
+import TopRatedDoctors from "@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <Specialties />
+      <TopRatedDoctors />
     </>
   );
 };
