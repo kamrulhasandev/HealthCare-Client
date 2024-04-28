c13995ae9ea18223531c2dc43ad1043db7a5156e
kamrul <kamrul444216@gmail.com>
2024-04-29 04:09:44 +0600

    Why Choose us section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
 import Specialties from "@/components/UI/HomePage/Specialties/Specialties";
 import TopRatedDoctors from "@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
+import WhyUs from "@/components/UI/HomePage/WhyUs/WhyUs";
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       <HeroSection />
       <Specialties />
       <TopRatedDoctors />
+      <WhyUs/>
     </>
   );
 };
