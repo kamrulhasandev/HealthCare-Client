ce8da148ba9bcdce110782406e5ce51f3375d19d
kamrul <kamrul444216@gmail.com>
2024-05-02 23:44:04 +0600

    create login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,107 @@
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+import assets from "@/assets";
+import Link from "next/link";
+
 const LoginPage = () => {
   return (
-    <div>
-      <h1>This is LoginPage</h1>
-    </div>
-  )
+    <Container
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+            textAlign: "center",
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Image src={assets.svgs.logo} alt="logo" width={50} height={50} />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Login PH HealthCare
+              </Typography>
+            </Box>
+          </Stack>
+          <Box>
+            <form>
+              <Grid container spacing={2} my={1}>
+                <Grid item md={6}>
+                  <TextField
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+              </Grid>
+              <Typography
+                mb={1}
+                textAlign={"end"}
+                component="p"
+                fontWeight={200}
+              >
+                Forgot Password?
+              </Typography>
+              <Button
+                fullWidth={true}
+                sx={{
+                  margin: "10px 0",
+                }}
+              >
+                Login
+              </Button>
+              <Typography component="p" fontWeight={300}>
+                Don&apos;t have an account?{" "}
+                <Link className="text-blue-500" href={"/register"}>
+                  Create an account
+                </Link>
+              </Typography>
+            </form>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
+  );
 };
 
 export default LoginPage;
