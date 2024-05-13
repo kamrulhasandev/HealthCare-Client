9af4ec283613c29fe91c148a9a362d46fb09252c
kamrul <kamrul444216@gmail.com>
2024-05-14 01:41:12 +0600

    date formatting

--- src/app/(withDashboardLayout)/dashboard/admin/schedules/components/SchedulesModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/components/SchedulesModal.tsx
@@ -1,5 +1,6 @@
 import FileUploader from "@/components/Forms/FileUploader";
 import PHDatePicker from "@/components/Forms/PHDatePicker";
+import PHTimePicker from "@/components/Forms/PHTimePicker";
 import PHForm from "@/components/Forms/PHForm";
 import PHInput from "@/components/Forms/PHInput";
 import PHModal from "@/components/Shared/PHModal/PHModal";
@@ -10,6 +11,7 @@ import { Button, CircularProgress, Grid } from "@mui/material";
 import React from "react";
 import { FieldValues } from "react-hook-form";
 import { toast } from "sonner";
+import { dateFormatter } from "@/utils/dateFormatter";
 
 type TProps = {
   open: boolean;
@@ -25,14 +27,32 @@ const SchedulesModal = ({
   setIsSubmitting,
 }: TProps) => {
   const handleFormSubmit = async (values: FieldValues) => {
+    values.startDate = dateFormatter(values.startDate);
+    values.endDate = dateFormatter(values.endDate);
+
     console.log(values);
   };
   return (
     <PHModal open={open} setOpen={setOpen} title={"Create A New Schedule"}>
       <PHForm onSubmit={handleFormSubmit}>
-        <Grid container spacing={2}>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            width: "400px",
+          }}
+        >
           <Grid item md={12}>
-            <PHDatePicker name="startDate" />
+            <PHDatePicker name="startDate" label="Start Date" />
+          </Grid>
+          <Grid item md={12}>
+            <PHDatePicker name="endDate" label="End Date" />
+          </Grid>
+          <Grid item md={6}>
+            <PHTimePicker name="startTime" label="Start Time" />
+          </Grid>
+          <Grid item md={6}>
+            <PHTimePicker name="endTime" label="End Time" />
           </Grid>
         </Grid>
         <Button disabled={isSubmitting} type="submit" sx={{ mt: 1 }}>
