8f2b5e501a85712915fcdde8e5e1d8732d0fa186
kamrul <kamrul444216@gmail.com>
2024-05-14 02:22:50 +0600

    display schedule data in admin dashboard

--- src/redux/api/scheduleApi.ts
+++ src/redux/api/scheduleApi.ts
@@ -1,31 +1,35 @@
+
 import { TagTypes } from "../tagTypes";
 import { baseApi } from "./baseApi";
 import { IMeta } from "@/types";
 
-const scheduleApi = baseApi.injectEndpoints({
+export const scheduleApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     createSchedule: build.mutation({
       query: (data) => ({
         url: "/schedule",
         method: "POST",
-        data: data,
+        data,
       }),
       invalidatesTags: [TagTypes.schedule],
     }),
     getAllSchedules: build.query({
-      query: (arg: Record<string, any>) => ({
-        url: "/schedule",
-        method: "GET",
-        params: arg,
-      }),
+      query: (arg: Record<string, any>) => {
+        return {
+          url: "/schedule",
+          method: "GET",
+          params: arg,
+        };
+      },
       transformResponse: (response: [], meta: IMeta) => {
         return {
-          doctors: response,
+          schedules: response,
           meta,
         };
       },
       providesTags: [TagTypes.schedule],
     }),
+
     deleteSchedule: build.mutation({
       query: (id) => ({
         url: `/schedule/${id}`,
